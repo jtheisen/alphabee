984c58cf378f36353abe64e88a1965a4be6c6f7b
Jens Theisen <pass-github@zokfotpik.de>
2025-12-24 15:20:41 +0100

    Initial commit

---
+++ Core/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace AlphaBee;
+
+public static class Extensions
+{
+	public static Int32 IndexOfBitCore(this Span<UInt64> words, UInt64 pattern)
+	{
+		var passed = 0;
+		foreach (var word in words)
+		{
+			var count = BitOperations.LeadingZeroCount(word ^ pattern);
+
+			if (count < 64)
+			{
+				return passed + count;
+			}
+
+			passed += 64;
+		}
+
+		return -1;
+	}
+
+	public static Int32 IndexOfBitZero(this Span<UInt64> words)
+		=> words.IndexOfBitCore(UInt64.MaxValue);
+
+	public static Int32 IndexOfBitOne(this Span<UInt64> words)
+		=> words.IndexOfBitCore(0);
+}
+
+//ref struct PageHelper
+//{
+//	Int32 wordsPerObject;
+
+//	Span<Byte> bytes;
+//	Span<Int64> words;
+
+//	public void Find()
+//	{
+//		BitOperations.LeadingZeroCount()
+//	}
+//}
---
+++ TestsWithXUnit/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using FsCheck.Xunit;
+using System.Buffers.Binary;
+
+namespace AlphaBee;
+
+public class UnitTest1
+{
+	[Property]
+	public bool ReverseOfReverseIsOriginal(UInt64[] words)
+	{
+		var i = words.AsSpan().IndexOfBitOne();
+		
+		if (i < 0) return true;
+
+		var word = words[i / 64];
+
+		var reversedWord = BinaryPrimitives.ReverseEndianness(word);
+
+		var shift = i % 64;
+
+		return (reversedWord >> shift) == 1;
+	}
+}
